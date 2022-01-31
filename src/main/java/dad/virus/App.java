b84989a6de6fbebfdb23ceaedc2c5633d0c712b4
Darel M <cls18drl@gmail.com>
2022-01-31 12:07:05 +0000

    Modificación del diseño. Añadido  musica de fondo

--- src/main/java/dad/virus/App.java
+++ src/main/java/dad/virus/App.java
@@ -6,12 +6,13 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class App extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
-		
 	}
 
 	private RootController controller;
--- src/main/java/dad/virus/RootController.java
+++ src/main/java/dad/virus/RootController.java
@@ -1,6 +1,7 @@
 package dad.virus;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,13 +10,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
-public class RootController implements Initializable{
+public class RootController implements Initializable {
 
-	@FXML
+    @FXML
     private Button configButton;
 
     @FXML
@@ -35,7 +41,14 @@ public class RootController implements Initializable{
 
     @FXML
     private BorderPane view;
-	
+
+    //JavaFX Media
+    AudioClip audioClip;
+    Media backgroundMusic;
+
+    @FXML
+    private Rectangle rectangleLogo;
+
     @FXML
     public void configAction(ActionEvent event) {
 
@@ -56,16 +69,48 @@ public class RootController implements Initializable{
 
     }
 
-	public RootController() throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml"));
-		loader.setController(this);
-		loader.load();
-	}
-	public BorderPane getView() {
-		return view;
-	}
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
+    public RootController() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml"));
+        loader.setController(this);
+        loader.load();
+    }
+
+    public BorderPane getView() {
+        return view;
+    }
+
+    private void setImageLogo(){
+        //Imágen logo
+        Image img = new Image(this.getClass().getResource("/image/viruslogo.jpg").toString());
+        rectangleLogo.setFill(new ImagePattern(img));
+    }
+
+    public void backgroundMusic() {
+        //Musica de fondo
+        try {
+            backgroundMusic = new Media(App.class.getResource("/media/background-music.mp3").toURI().toString());
+            audioClip = new AudioClip(backgroundMusic.getSource());
+
+            //TODO Realizar listener del slider para el control del volumen
+
+            audioClip.setVolume(0.1f); // Volumen entre 0 y 1
+            audioClip.play();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void playMedia(){
+        audioClip.play();
+    }
+
+    public void pauseMedia(){
+        audioClip.stop();
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+       setImageLogo();
+       backgroundMusic();
+    }
 }
