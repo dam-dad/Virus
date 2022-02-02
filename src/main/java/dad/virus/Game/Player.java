6f75fee649c4f1061ffe1a11e12aa7691a0ede03
Cristo Chacon <cristochacon88@gmail.com>
2022-02-02 14:15:51 +0000

    Create Player.java

---
+++ src/main/java/dad/virus/Game/Player.java
@@ -0,0 +1,41 @@
+package dad.virus.Game;
+
+import dad.virus.virus.Card;
+import dad.virus.virus.Organ;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    private ArrayList<Organ> body;
+    private ArrayList<Card> hand;
+
+    public Player() {
+        this.body = new ArrayList<>();
+        this.hand = new ArrayList<>();
+    }
+
+    public ArrayList<Organ> getBody() {
+        return body;
+    }
+
+    public void setBody(Organ organ) {
+        if (this.body == null) {
+            this.body.add(organ);
+        } else {
+            for (int i = 0; i < this.body.size(); i++) {
+                if (!this.body.get(i).getColor().equals(organ.getColor())) {
+                    this.body.add(organ);
+                }
+            }
+        }
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+}
